026874ac2d5d135188fb205f773acdf2e0b5af39
8467042733 <rupeshpal25112004@gmail.com>
2024-01-11 15:46:31 +0530

    add some code

--- src/app.js
+++ src/app.js
@@ -15,7 +15,37 @@ app.use(express.json());
 // Write PATCH endpoint to buy a product for the client here
 // Endpoint /api/v1/products/:id
 
-
+app.patch('/api/v1/products/:id', (req, res) => {
+    const productId = parseInt(req.params.id);
+    const productIndex = products.findIndex(product => product.id === productId);
+    if (productIndex !== -1) {
+        const product = products[productIndex];
+        if (product.quantity > 0) {
+            product.quantity--;
+            products[productIndex] = product;
+            res.status(200).json({
+                status: 'success',
+                message: `Thank you for purchasing ${product.name}`,
+                product: {
+                    id: product.id,
+                    name: product.name,
+                    price: product.price,
+                    quantity: product.quantity,
+                },
+            });
+        } else {
+            res.status(404).json({
+                status: 'success',
+                message: `${product.name}, Out of stock!`,
+            });
+        }
+    } else {
+        res.status(404).json({
+            status: 'failed',
+            message: 'Product not found!',
+        });
+    }
+});
 
 
 module.exports = app;
